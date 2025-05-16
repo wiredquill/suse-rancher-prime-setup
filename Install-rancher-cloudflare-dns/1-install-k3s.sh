4d4a0f1197a9953347d935ada727254f31b85fa6
wiredquill <erin@suse.com>
2025-05-15 19:48:47 -0700

    Update 1-install-k3s.sh

--- Install-rancher-cloudflare-dns/1-install-k3s.sh
+++ Install-rancher-cloudflare-dns/1-install-k3s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Install K3s without Traefik
+# Install K3s 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.31.6+k3s1" sh -
 
 # Make the kubeconfig readable for erin
